381809c0bfa77d8d3d4c7d2c8a0ea081bfa741b8
Alejandro Mery <amery@jpi.io>
2023-05-26 17:54:21 +0000

    docker: add mariadb container
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- Makefile
+++ Makefile
@@ -6,7 +6,8 @@ SHELL = /bin/sh
 
 # generated outputs
 #
-FILES = docker-compose.yml
+FILES = docker-compose.yml \
+	db.env
 
 CONFIG_MK = config.mk
 GEN_MK = gen.mk
--- config.mk
+++ config.mk
@@ -1,3 +1,11 @@
 TRAEFIK_BRIDGE ?= traefiknet
+USER_GID ?= $(shell id -ur)
+USER_UID ?= $(shell id -gr)
 NAME ?= whoami
 HOSTNAME ?= $(NAME).docker.localhost
+MYSQL_IMAGE ?= amery/docker-alpine-mariadb
+MYSQL_SERVER ?= db
+MYSQL_DATABASE ?= $(NAME)
+MYSQL_USER ?= $(MYSQL_DATABASE)
+MYSQL_PASSWORD ?= secret1
+MYSQL_ROOT_PASSWORD ?= secret2

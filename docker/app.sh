3f9e9e593ce495be1ea2516d003646eb2a51bb64
Alejandro Mery <amery@jpi.io>
2024-02-12 20:28:42 +0000

    app: install wordpress instance if blank
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/app.sh
+++ docker/app.sh
@@ -13,6 +13,35 @@ query() {
 	mysql -h$DB_SERVER ${DB_PORT:+-P$DB_PORT} "-u${DB_USER:-root}" ${DB_PASSWD:+-p"$DB_PASSWD"} "$@"
 }
 
+# wp-cli
+#
+vendored="$PWD/vendor/wp-cli/wp-cli/bin/wp"
+if [ ! -x "$vendored" ]; then
+	F=composer.json
+	if [ ! -s "$F" ]; then
+		info "Generating $F..."
+		cat <<-EOT > "$F~"
+		{
+		  "require": {
+		  "wp-cli/wp-cli-bundle": "*"
+		  }
+		}
+		EOT
+		chown "appuser:appuser" "$F~"
+	        mv "$F~" "$F"
+	fi
+
+        info "Running composer ..."
+        run-user composer install --no-interaction
+fi
+
+WP=/usr/bin/wp
+[ -x "$WP" ] || ln -s "$vendored" "$WP"
+
+wp() {
+	run-user "$WP" "$@"
+}
+
 # wait for database
 #
 if [ -z "${DB_SERVER:-}" ]; then
@@ -32,3 +61,30 @@ while true; do
 	info "Waiting for confirmation of MariaDB service startup"
 	sleep 5
 done
+
+# wp-config.php
+#
+F=wp-config.php
+if [ ! -s "$F" ]; then
+	info "DB server $DB_SERVER is available, configuring Workdpress"
+
+	wp config create \
+		--color \
+		--dbname="$DB_NAME" \
+		--dbuser="$DB_USER" \
+		--dbpass="$DB_PASSWD" \
+		--dbhost="$DB_SERVER" \
+		${WP_LANG:+--locale=${WP_LANG}}
+
+	run-user wp core install \
+		--color \
+		--url="https://$VIRTUAL_HOST" \
+		--title="${WP_TITLE:-}" \
+		--admin_user="${WP_ADMIN_USER}" \
+		--admin_email="${WP_ADMIN_EMAIL}" \
+		${WP_LANG:+--locale=${WP_LANG}}
+
+	run-user wp plugin install backup-backup
+fi
+
+info "Almost there! Starting web server now"
